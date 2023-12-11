cb70fe1b782e8d7a910d7f6928184a54e98939d5
tshepoMsiza <isaact.msiza@gmail.com>
2023-12-11 07:59:39 +0200

    new changes

--- app/src/main/java/com/example/geekschathub/activities/MainActivity.java
+++ app/src/main/java/com/example/geekschathub/activities/MainActivity.java
@@ -11,22 +11,12 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-    Timer time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        time = new Timer();
-        time.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
-                startActivity(intent);
-
-            }
-        }, 10000);
 
     }
 }
